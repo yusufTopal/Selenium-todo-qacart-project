bea72f86a4d7b2437efd4ca9841c889299b4c4eb
yusuftopal <yusuftopal00@gmail.com>
2024-01-16 22:52:14 +0100

    Running chrome on headless mode

--- src/test/java/com/qacart/todo/factory/DriverFactory.java
+++ src/test/java/com/qacart/todo/factory/DriverFactory.java
@@ -2,6 +2,7 @@ package com.qacart.todo.factory;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
@@ -15,7 +16,9 @@ public class DriverFactory {
         WebDriver driver;
         switch (browser) {
             case "CHROME" -> {
-                driver = new ChromeDriver();
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--headless");
+                driver = new ChromeDriver(options);
             }
             case "FIREFOX" -> {
                 driver = new FirefoxDriver();
